ef7661e875de34f93b8c254d178ef9c8ade51333
Phyoe Thant Ko <84618795+PhyoeThantKo@users.noreply.github.com>
2022-04-17 19:49:05 +0630

    get_users_stats

--- routes/user.js
+++ routes/user.js
@@ -42,4 +42,42 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res)=>{
      };
 });
 
+//get all user
+router.get("/", verifyTokenAndAdmin, async (req, res)=>{
+     const query = req.query.new;
+     try{
+          const users = query ? await user.find().sort({_id:-1}).limit(5) : await user.find();
+
+          res.status(200).json(users);
+     }catch(err){
+          res.status(500).json(err);
+     };
+});
+
+//Get user stats
+router.get("/stats", verifyTokenAndAdmin, async (req,res)=>{
+     const date = new Date();
+     const lastYear = new Date(date.setFullYear(date.getFullYear()-1));
+
+     try{
+          const data = await user.aggregate([
+               {$match: {createdAt: {$gte: lastYear} } },
+               {
+                    $project:{
+                         month: {$month: "$createdAt"}
+                    },
+               },
+               {
+                    $group:{
+                         _id: "$month",
+                         total: {$sum: 1},
+                    }
+               }
+          ]);
+          return res.status(200).json(data);
+     }catch(err){
+          res.status(500).json(err);
+     }
+});
+
 module.exports = router;
